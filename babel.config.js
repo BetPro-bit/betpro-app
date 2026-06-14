26a8a0cde7f9f58ec7ff46e14b89acd2c513300b
BetPro-bit <cbhmediake@gmail.com>
2026-06-14 14:45:55 +0300

    Disable reanimated plugin on web

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,12 @@
 module.exports = function (api) {
   api.cache(true);
+
+  const isWeb = process.env.EXPO_TARGET === 'web' || 
+                process.env.BABEL_ENV === 'web' ||
+                process.env.NODE_ENV === 'production';
+
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      'react-native-reanimated/plugin',
-    ],
+    plugins: isWeb ? [] : ['react-native-reanimated/plugin'],
   };
 };
