a87eec17ef72ab3298938b36febc96ba48f86915
BetPro-bit <cbhmediake@gmail.com>
2026-06-14 14:55:12 +0300

    Remove reanimated - not used in app

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,6 @@
 module.exports = function (api) {
   api.cache(true);
-
-  const isWeb = process.env.EXPO_TARGET === 'web' || 
-                process.env.BABEL_ENV === 'web' ||
-                process.env.NODE_ENV === 'production';
-
   return {
     presets: ['babel-preset-expo'],
-    plugins: isWeb ? [] : ['react-native-reanimated/plugin'],
   };
 };
